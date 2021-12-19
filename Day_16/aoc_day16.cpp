af698024fcd8120b22aea91a820965659efe1690
krauluk1 <krauluk1@gmail.com>
2021-12-19 23:51:23 +0100

    day 16 solution

--- Day_16/aoc_day16.cpp
+++ Day_16/aoc_day16.cpp
@@ -8,25 +8,39 @@
 #include <vector>
 #include <algorithm>
 
+using ushort = unsigned short;
+using ull_int = unsigned long long int;
 
-class PacketDecoder
-{
+class PacketDecoder{
     private:
         std::string binary;
         void read_txt(const std::string path);
         const char* hex_to_bin(char c);
-        void PacketDecoder::parse_package();
+        ull_int parse_packet();
+        int sum_version = 0;
+        ull_int express;
 
     public: 
         PacketDecoder(std::string path);
+        int get_sum_version();
+        ull_int get_expression();
 };
 
 PacketDecoder::PacketDecoder(std::string path){
     read_txt(path);
+    express = parse_packet();
 }
 
-void PacketDecoder::parse_package(){
-    // package Version
+ull_int PacketDecoder::get_expression(){
+    return express;
+}
+
+int PacketDecoder::get_sum_version(){
+    return sum_version;
+}
+
+ull_int PacketDecoder::parse_packet(){
+    // Version
     std::string str_version{binary.begin(), binary.begin() + 3};
 	binary.erase(binary.begin(), binary.begin() + 3);
     int version = std::stoi(str_version, nullptr, 2);
@@ -36,10 +50,84 @@ void PacketDecoder::parse_package(){
 	binary.erase(binary.begin(), binary.begin() + 3);
     int id = std::stoi(str_id, nullptr, 2);
 
-    if(id == 4){
-        
-    }
+	// Task 1: Add all versions
+    sum_version += version;
 
+	if (id == 4){
+		// Literal value, description in task 1 
+		// Binary number is padded with zeros 
+		bool last_pkg = false;
+		std::string value;
+		while (!last_pkg){
+			// If leading is a zero, it is the last pkg
+			// Others are paddings
+			if (binary.front() == '0')
+				last_pkg = true;
+			// Delete description
+			binary.erase(binary.begin());
+			// Data of pkg
+			value.append({ binary.begin(), binary.begin() + 4 });
+			binary.erase(binary.begin(), binary.begin() + 4);
+		}
+		// Change value to
+		return std::stoll(value, nullptr, 2);
+	}
+	else
+	{
+		// Operator, see task 1 for description
+		// Contains one or more pkg
+		std::vector<ull_int> sub_packets;
+
+		// ID = 0 => 15 bits
+		// ID = 1 => 11 bits
+		char lenght_type_id{binary.front()};
+		binary.erase(binary.begin());
+		if (lenght_type_id == '0'){
+
+			// Get total length of bits
+			std::string lenght_str{ binary.begin(), binary.begin() + 15 };
+			binary.erase(binary.begin(), binary.begin() + 15);
+			ushort lenght = std::stoi(lenght_str, nullptr, 2);
+
+			// Get Sub-Packets 
+			size_t curr_size = binary.size();
+			while (binary.size() > curr_size - lenght){
+				sub_packets.push_back(parse_packet());
+			}
+		}else{
+			std::string packet_nb_str{ binary.begin(), binary.begin() + 11 };
+			binary.erase(binary.begin(), binary.begin() + 11);
+			ushort packet_nb = std::stoi(packet_nb_str, nullptr, 2);
+
+			for (ushort i = 0; i < packet_nb; i++)
+				sub_packets.push_back(parse_packet());
+		}
+
+		if (id == 0)
+		{
+			ull_int value = 0;
+			for (auto& i : sub_packets)
+				value += i;
+			return value;
+		}
+		else if (id == 1)
+		{
+			ull_int value = 1;
+			for (auto& i : sub_packets)
+				value *= i;
+			return value;
+		}
+		else if (id == 2)
+			return *std::min_element(sub_packets.begin(), sub_packets.end());
+		else if (id == 3)
+			return *std::max_element(sub_packets.begin(), sub_packets.end());
+		else if (id == 5)
+			return (ull_int)(sub_packets[0] > sub_packets[1]);
+		else if (id == 6)
+			return (ull_int)(sub_packets[0] < sub_packets[1]);
+		else if (id == 7)
+			return (ull_int)(sub_packets[0] == sub_packets[1]);
+	}
 }
 
 void PacketDecoder::read_txt(const std::string path){
@@ -50,15 +138,14 @@ void PacketDecoder::read_txt(const std::string path){
     while (infile >> line)
     {
         for(char c:line){
+			// Change Hex to binary
            binary.append(hex_to_bin(c)); 
         }
     }
 }
 
-const char* PacketDecoder::hex_to_bin(char c)
-{
-	switch (c)
-	{
+const char* PacketDecoder::hex_to_bin(char c){
+	switch (c){
 	case '0': return "0000";
 	case '1': return "0001";
 	case '2': return "0010";
@@ -78,9 +165,11 @@ const char* PacketDecoder::hex_to_bin(char c)
 	}
 }
 
-int main()
-{
+int main(){
     PacketDecoder d("input.txt");
-    std::cout << "\nSolution taks 1: " << "" << std::endl;
+    int t1 = d.get_sum_version();
+    ull_int t2 = d.get_expression();
+    std::cout << "\nSolution taks 1: " << t1 << std::endl;
+    std::cout << "\nSolution taks 2: " << t2 << std::endl;
     return 0;
 }
